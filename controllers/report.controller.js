4eb1a21116de3d39d43488ce7748556f141740d7
tatevik <tatevik.senokyan@gmail.com>
2024-09-12 16:41:29 +0400

    add report part

--- controllers/report.controller.js
+++ controllers/report.controller.js
@@ -1,14 +1,28 @@
+const Task = require("../models/Tasks");
 
 exports.reports = async (req, res) => {
-    console.log("Start Report Generation", req.query);
-    const { startdate, deadline, assignee } = req.query;
+    console.log("Start Report Generation");
+    const { startdate, enddate, assignee } = req.query;
 
-    const data = {};
-    const jsonData = JSON.stringify(data, null, 2);
-    
-    res.setHeader('Content-Disposition', 'attachment; filename=report.json');
-    res.setHeader('Content-Type', 'application/json');
+    const query = {};
 
+    if (startdate) query.completed = { ...query.completed, $gte: new Date(startdate).getTime() };
+
+    if (assignee) query.assignee = assignee;
+
+    if (enddate)  query.completed = { ...query.completed, $lte: new Date(enddate).getTime() };
+
+    const data = await Task.find(query).select("-_id -updateHistory").exec();
+    const jsonData = { 
+        count: data.length,
+        items: data 
+    }
+
+    if (data.length && startdate && enddate && enddate>=startdate) {
+        jsonData.avgTime = Math.ceil((query.completed["$lte"] - query.completed["$gte"]) / (data.length*24*60*60*1000));
+    }
+
+    res.setHeader("Content-Disposition", "attachment; filename=report.json");
+    res.setHeader("Content-Type", "application/json");
     res.send(jsonData);
-  
 }
--- models/Tasks.js
+++ models/Tasks.js
@@ -12,12 +12,12 @@ const taskSchema = new Schema(
     {
         title: {type: String, required: [true, "task title is required"], lowercase: true},
         description: {type: String, required: true, lowercase: true},
-        assignee: {type: String, required: true, lowercase: true},
+        assignee: {type: String, required: true, lowercase: true, index: true},
         priority: {type: String, required: true, enum: ["high", "low", "normal"]},
         status: {type: String, required: true, enum: ["completed", "to do", "in progress"], default: "to do"},
         created: {type: Number, default: Date.now},
         deadline: {type: Number, required: true},
-        completed: {type: Number, default: null},
+        completed: {type: Number, default: null, index: true},
         updateHistory: [activitySchema] 
     }, 
     {versionKey: false}  
